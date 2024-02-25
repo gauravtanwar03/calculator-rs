cf4a31a4b0ddd7980755c8c7727dabe9cf700c27
Gaurav Kuamr <gaurav.kumar@moonshinetechnology.com>
2024-02-25 11:52:37 +0530

    Added operate function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::env::{args, Args};
 
+fn operate(operator: &str, first: f32, second: f32) -> f32 {
+    match operator {
+        "+" => first + second,
+        "-" => first - second,
+        "*" | "X" | "x" => first * second,
+        "/" => first / second,
+        _ => panic!("Invalid operator"),
+    }
+}
+
 fn main() {
     let mut args: Args = args();
     let first = args.nth(1).unwrap();
@@ -8,6 +18,7 @@ fn main() {
 
     let first_number = first.parse::<f32>().unwrap();
     let second_number = second.parse::<f32>().unwrap();
+    let result = operate(&operator, first_number, second_number);
 
     println!("{} {} {}", first_number, operator, second_number)
 }
