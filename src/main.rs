d3c10d3d3e6d50c78f8c3c1673fc03440602516c
Gaurav Kuamr <gaurav.kumar@moonshinetechnology.com>
2024-02-25 11:47:36 +0530

    Added command arguments support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    println!("{} {} {}", first, operator, second)
 }
